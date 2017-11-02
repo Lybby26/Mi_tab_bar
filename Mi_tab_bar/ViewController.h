965c39b14e9fc7d6c36445865c611bcaa0084d7b
Mac Mini <macmini@macs-mac-mini.local>
2017-11-01 18:36:21 -0600

    Initial Commit

---
+++ Mi_tab_bar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mi_tab_bar
+//
+//  Created by Mac Mini on 11/1/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mi_tab_bar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Mi_tab_bar
+//
+//  Created by Mac Mini on 11/1/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
