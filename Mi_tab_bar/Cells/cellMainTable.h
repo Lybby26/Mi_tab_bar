0c3bb5b50a3be78517c9056421afb62fed532edc
Mac Mini <macmini@macs-mac-mini.local>
2017-11-22 16:42:43 -0600

    agregando tabla y celda

---
+++ Mi_tab_bar/Cells/cellMainTable.h
@@ -0,0 +1,17 @@
+//
+//  cellMainTable.h
+//  Mi_tab_bar
+//
+//  Created by Mac Mini on 11/22/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *lblFecha;
+@property (strong, nonatomic) IBOutlet UILabel *lblHora;
+@property (strong, nonatomic) IBOutlet UILabel *lblEquipos;
+@property (strong, nonatomic) IBOutlet UILabel *lblEstadio;
+
+@end
